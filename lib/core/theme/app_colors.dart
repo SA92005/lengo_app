1d7051752d5f7d5d35bb8867cfd0f424929d564f
SA92005 <sa9432216@gmail.com>
2026-07-25 16:31:13 +0300

    create dev branch

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff6247BF);
+  static const Color backgroundColor = Color(0xffFFFFFE);
+  static const Color photoColor = Color(0xff7B65C9);
+  static const Color greyColor = Color(0xff797685);
+}
